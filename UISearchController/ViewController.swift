f6e8d5af256ee5b6790a24248302f674caab986c
aliya <tlegen.aliya@gmail.com>
2022-09-01 23:42:17 +0600

    Initial Commit

---
+++ UISearchController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISearchController
+//
+//  Created by Алия Тлеген on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
